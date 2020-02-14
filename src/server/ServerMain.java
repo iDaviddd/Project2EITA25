64b9f2bf6a49a22d1d88ca54b7e91bbe41e25751
David Öhrbom <david@ohrbom.se>
2020-02-14 12:56:46 +0100

    Important changes, we use java 13 for the project.

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -3,7 +3,9 @@ package server;
 public class ServerMain {
 	
 	public static void main(String[] args) {
+		System.out.println("Server starting...");
 		
+		System.out.println("Server started successfully");
 	}
 
 }
