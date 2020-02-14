592ec76c87ca37eb3ac3d720acd7208ea0bb6bc5
David Öhrbom <david@ohrbom.se>
2020-02-14 13:33:23 +0100

    Added database folder where database will be stored.

--- src/server/database/DatabaseHandler.java
+++ src/server/database/DatabaseHandler.java
@@ -1,5 +1,16 @@
 package server.database;
 
 public class DatabaseHandler {
+	
+	private String dbName;
+	
+	public DatabaseHandler(String dbName) {
+		this.dbName = dbName;
+		initializeDB();
+	}
+	
+	public void initializeDB() {
+		
+	}
 
 }
