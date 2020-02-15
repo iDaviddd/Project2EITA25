4d555652f6c107acf447516b0f0fb68641c01853
David Öhrbom <david@ohrbom.se>
2020-02-15 23:31:57 +0100

    Trying some database techniques.

--- src/server/database/DatabaseHandler.java
+++ src/server/database/DatabaseHandler.java
@@ -1,16 +1,33 @@
 package server.database;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseHandler {
-	
+
 	private String dbName;
-	
+
 	public DatabaseHandler(String dbName) {
 		this.dbName = dbName;
 		initializeDB();
 	}
-	
+
 	public void initializeDB() {
-		
+		String url = "jdbc:sqlite:/Users/dohrbom/java-workspace/EITA25 Project 2/database/" + dbName;
+
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			if (conn != null) {
+				DatabaseMetaData meta = conn.getMetaData();
+				System.out.println("The driver name is " + meta.getDriverName());
+				System.out.println("A new database has been created.");
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
