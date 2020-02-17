9521099af50a095de7932a37aa31d6e3d8437556
David Öhrbom <david@ohrbom.se>
2020-02-17 15:32:36 +0100

    Now has working database, some tweaks left though.

--- src/server/database/DatabaseHandler.java
+++ src/server/database/DatabaseHandler.java
@@ -19,13 +19,13 @@ public class DatabaseHandler {
 		initializeDB();
 	}
 
-	public void initializeDB() {
+	private void initializeDB() {
 		String url = "jdbc:sqlite:/Users/dohrbom/java-workspace/EITA25 Project 2/database/" + dbName + ".db";
 		try {
 			this.conn = DriverManager.getConnection(url);
 			if (conn != null) {
 				DatabaseMetaData meta = conn.getMetaData();
-				System.out.println("The driver name is " + meta.getDriverName());
+//				System.out.println("The driver name is " + meta.getDriverName());
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
@@ -37,52 +37,59 @@ public class DatabaseHandler {
 		String url = "jdbc:sqlite:/Users/dohrbom/java-workspace/EITA25 Project 2/database/" + dbName + ".db";
 
 		// SQL statement for creating a new table
-		String sql1 = "CREATE TABLE IF NOT EXISTS employees (\n" + " uuid integer PRIMARY KEY,\n"
-				+ " name text NOT NULL,\n" + " capacity real\n" + ");";
-		String sql2 = "CREATE TABLE IF NOT EXISTS users (\n" + " id integer PRIMARY KEY, \n"
-				+ " firstname VARCHAR(20),\n" + " lastname VARCHAR(20),\n"
-				+ " role VARCHAR(20),\n" + " username VARCHAR(20),\n" + " firstname VARCHAR(20),\n"
-				+ " password text NOT NULL,\n" + " salt text NOT NULL,\n" + " division VARCHAR(20)\n" + ");";
-		String sql3 = "CREATE TABLE IF NOT EXISTS records (\n" + " record_id integer PRIMARY KEY,\n"
-				+ " patient_id INT NOT NULL,\n" + " doctor_id INT NOT NULL,\n"
-				+ " nurse_id INT NOT NULL" + " division VARCHAR(20),\n";
-		String sql4 = "CREATE TABLE IF NOT EXISTS logs (\n" + " timestamp VARCHAR(20),\n"
-				+ "used_id INT NOT NULL";
+		String sql1 = "CREATE TABLE IF NOT EXISTS users (\n" + " id integer PRIMARY KEY, \n"
+				+ " firstname VARCHAR(20),\n" + " lastname VARCHAR(20),\n" + " role VARCHAR(20) NOT NULL,\n"
+				+ " username VARCHAR(20) NOT NULL,\n" + " password text NOT NULL,\n" + " salt text NOT NULL,\n"
+				+ " division VARCHAR(20)\n" + ");";
+		String sql2 = "CREATE TABLE IF NOT EXISTS records (\n" + " record_id integer PRIMARY KEY,\n"
+				+ " patient_id INT NOT NULL,\n" + " doctor_id INT NOT NULL,\n" + " nurse_id INT NOT NULL,\n"
+				+ " division VARCHAR(20)\n" + ");";
+		String sql3 = "CREATE TABLE IF NOT EXISTS logs (\n" + "log_id integer PRIMARY KEY,\n" + "timestamp VARCHAR(20),\n"
+				+ "used_id INT NOT NULL,\n" + "record_id INT NOT NULL,\n" + "action_type VARCHAR(20),\n"
+				+ "action VARCHAR(255)\n" + ");";
 		try {
 			Connection conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement();
-//			stmt.execute(sql);
+			stmt.execute(sql1);
+			stmt.execute(sql2);
+			stmt.execute(sql3);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	public void insert(String name, double capacity) {
-		String sql = "INSERT INTO employees(name, capacity) VALUES(?,?)";
+	public void addUser(String firstname, String lastname, String role, String username, String password, String salt,
+			String division) {
+		String sql = "INSERT INTO users(firstname, lastname, role, username, password, salt, division) VALUES(?,?,?,?,?,?,?)";
 
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, name);
-			pstmt.setDouble(2, capacity);
+			pstmt.setString(1, firstname);
+			pstmt.setString(2, lastname);
+			pstmt.setString(3, role);
+			pstmt.setString(4, username);
+			pstmt.setString(5, password);
+			pstmt.setString(6, salt);
+			pstmt.setString(7, division);
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	public void delete(String name) {
-		String sql = "DELETE FROM employees WHERE name='" + name + "';";
-		
+
+	public void deleteUser(int id) {
+		String sql = "DELETE FROM users WHERE id='" + id + "';";
+
 		try {
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			stmt.execute(sql);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	public void selectAll() {
-		String sql = "SELECT * FROM employees";
+	public void printAllUsers() {
+		String sql = "SELECT * FROM users";
 
 		try {
 			Statement stmt = conn.createStatement();
@@ -90,16 +97,14 @@ public class DatabaseHandler {
 
 			// loop through the result set
 			while (rs.next()) {
-				System.out.println(rs.getInt("id") + "\t" + rs.getString("name") + "\t" + rs.getDouble("capacity"));
+				System.out.println(rs.getInt("id") + "\t" + rs.getString("firstname") + "\t"
+						+ rs.getString("lastname") + "\t" + rs.getString("role") + "\t" + rs.getString("username")
+						+ "\t" + rs.getString("password") + "\t" + rs.getString("salt") + "\t"
+						+ rs.getString("division"));
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-	}
-
-	public static void testDB() {
-		DatabaseHandler dbh = new DatabaseHandler("records");
-		dbh.selectAll();
 	}
 
 }
